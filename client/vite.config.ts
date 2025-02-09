328ade949947297af0ecc77cd567bfe39732c2eb
Alinde Öst <ost.alinde@gmail.com>
2025-02-09 18:03:04 +0100

    added proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: isProduction
+          ? "https://portfolio-obil.onrender.com" // production
+          : "http://localhost:3000", //dev
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
